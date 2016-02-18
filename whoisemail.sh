cdbfa92db6d6e41097ce81deb8a748020448e4e9
Jmoore53 <jmoore6786wjm@hotmail.com>
2016-02-17 19:41:30 -0500

    rename

--- whoisemail.sh
+++ whoisemail.sh
@@ -1,21 +1,24 @@
 #!/bin/bash/
-
-
 #Bash Script that takes a file of web domains and returns email addresses
+clear
 
-echo "Starting Script."
-echo ""
+echo "Starting Script.";
 
-FILE=$1
+FILE=$1;
+declare -a WEBSITES;
+count=0;
 
 echo "The file given was $FILE"
+echo ""
 
 while IFS='' read -r line || [[ -n "$line" ]]; do
-	echo $line
+	WEBSITES[count]=$line #Sets the line from the file equal to an index in the array
+    #WEBSITES[count]=($(whois $line | grep 'Email:')) #does not work, cant assign list to array value
+    echo "Website: "$line
     whois $line | grep 'Email:'
+    echo ""
+    count=$(( $count + 1 ))
 done < "$FILE"
-
-
 # Description
 # IFS='' (or IFS=) prevents leading/trailing whitespace from being trimmed.
 # '-r' prevents backslash escapes from being interpreted.
@@ -23,12 +26,11 @@ done < "$FILE"
 # '\n' (since read returns a non-zero exit code when it encounters EOF).
 
 
+echo "Websites in $FILE"
+#echo ${WEBSITES[*]}
+for i in ${WEBSITES[@]}; do echo $i; done;
+
+
 #echo $ARRAY[0]
 
-
-
-
-
 echo ""
-
-
